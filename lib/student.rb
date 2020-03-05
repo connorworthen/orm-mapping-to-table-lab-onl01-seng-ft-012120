be763d71841a42379eb7c53060acfb47a15bb8b9
connor worthen <connorw220@protonmail.com>
2020-03-05 20:18:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,40 +1,49 @@
 class Student
 
-  attr_accessor :name, :grade, :id
+  attr_accessor :name, :grade
+  attr_reader :id
   
   def initialize(name, grade, id=nil)
-    @id = id
     @name = name
     @grade = grade
+    @id = id
   end
-  
+
   def self.create_table
     sql = <<-SQL
-      CREATE TABLE IF NOT EXISTS students (
+    CREATE TABLE IF NOT EXISTS students (
       id INTEGER PRIMARY KEY,
       name TEXT,
-      grade TEXT,
-      )
-      SQL
+      grade INTEGER
+    )
+    SQL
     DB[:conn].execute(sql)
   end
- 
+
+
+  def self.drop_table
+    sql = <<-SQL
+    DROP TABLE IF EXISTS students
+    SQL
+    DB[:conn].execute(sql)
+  end
+
   def save
     sql = <<-SQL
-      INSERT INTO songs (name, grade) 
-      VALUES (?, ?)
+    INSERT INTO students (name, grade)
+    VALUES (?, ?)
     SQL
- 
     DB[:conn].execute(sql, self.name, self.grade)
-    
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
- 
-  def self.create(name, album)
-    song = Song.new(name, album)
-    song.save
-    song
+  
+
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
   end
+
 end
   
 
